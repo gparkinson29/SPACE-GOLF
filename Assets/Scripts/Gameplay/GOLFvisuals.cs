87f51657d2718f9fb9f749ba2cd7d789ec318769
Eric B <40864286+EBahamJr@users.noreply.github.com>
2024-03-31 22:43:24 -0400

    bug fixes, please revert any errant gameplay starters to the prefab

--- Assets/Scripts/Gameplay/GOLFvisuals.cs
+++ Assets/Scripts/Gameplay/GOLFvisuals.cs
@@ -6,12 +6,18 @@ public class GOLFvisuals : MonoBehaviour
 {
     public GameObject player;
     public float smoothness = 1f;
+    public LevelController lvlController;
+
+    private void Awake()
+    {
+        lvlController = GameObject.FindGameObjectWithTag("LVLcontroller").GetComponent<LevelController>();
+    }
 
     private void Update()
     {
         transform.position = player.transform.position;
         Vector3 vel = player.GetComponent<Rigidbody>().velocity;
-        if (vel != Vector3.zero)
+        if (lvlController.GetGameState() != LevelController.gameState.putting)
         {
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(-vel, Vector3.down), smoothness); //Quaternion.LookRotation(-vel, Vector3.down);
         }
